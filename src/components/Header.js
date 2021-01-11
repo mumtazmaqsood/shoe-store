7a72df9b0ad0db2d3efee993a3dd9de097f43361
Mumtaz <mumtaz.maqsood@yahoo.com>
2021-01-11 20:25:34 +0100

    nav bar created

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <hr />
+            <ul>
+                <li><h1>Shoe Store</h1></li>
+                <li>Home </li>
+                <li>About </li>
+                <li>Products </li>
+            </ul>
+            <hr />
+        </div>
+
+    )
+}
+
